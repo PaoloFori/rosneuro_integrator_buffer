45ed0558bca7d186ad2b8ab3e90155ffcde19a06
PaoloFori <forin.paolo98@gmail.com>
2024-07-31 15:15:10 +0200

    new integrator buffer with a new implementation similar to the bars

--- include/rosneuro_integrator_buffer/Buffer.h
+++ include/rosneuro_integrator_buffer/Buffer.h
@@ -3,55 +3,56 @@
 
 #include <Eigen/Dense>
 #include <ros/ros.h>
+#include <numeric>
+#include <random>
 #include <pluginlib/class_list_macros.h>
 #include <dynamic_reconfigure/server.h>
 #include "rosneuro_integrator/GenericIntegrator.h"
 #include "rosneuro_integrator_buffer/BufferConfig.h"
 
-#define INCREMENT_HARD 0
-#define INCREMENT_SOFT 1
-
 namespace rosneuro {
-	namespace integrator {
+    namespace integrator {
 
 using rosneuro_config_buffer = rosneuro_integrator_buffer::BufferConfig;
 using dyncfg_buffer         = dynamic_reconfigure::Server<rosneuro_config_buffer>;
 
 class Buffer : public GenericIntegrator {
 
-	public:
-		Buffer(void);
-		~Buffer(void);
+    public:
+        Buffer(void);
+        ~Buffer(void);
 
-		bool configure(void);
-		Eigen::VectorXf apply(const Eigen::VectorXf& input);
-		bool reset(void);
-		void setClasses(int value);
-		void setIncrement(int value);
-		void setBuffersSize(std::vector<int> value);
-		void setInitVal(std::vector<float> init_val);
-		void setBufferSize(int value, int idx_buffer);
-		std::vector<float> getInitVal(void);
+        bool configure(void);
+        Eigen::VectorXf apply(const Eigen::VectorXf& input);
+        bool reset(void);
+        void setNClasses(int value);
+        void setClasses(std::vector<int> value);
+        void setThresholds(std::vector<float> value);
+        void setBufferSize(int value);
+        void setInitPercentual(std::vector<float> init_percentual);
+        std::vector<float> getInitPrecentual(void);
 
-	private:
-		Eigen::VectorXf uniform_vector(int size, float value);
-		void on_request_reconfigure(rosneuro_config_buffer &config, uint32_t level);
+    private:
+        Eigen::VectorXf uniform_vector(int size, float value);
+        void on_request_reconfigure(rosneuro_config_buffer &config, uint32_t level);
 
-	private:
-		ros::NodeHandle p_nh_;
-		Eigen::VectorXf data_;
-		int increment_;
-		int n_classes_;
-		std::vector<int> buffers_size_;
-		std::vector<float> init_val_;
-		dyncfg_buffer recfg_srv_;
-  		dyncfg_buffer::CallbackType recfg_callback_type_;
-		bool first_;
+    private:
+        ros::NodeHandle p_nh_;
+		Eigen::VectorXf buffer_;
+        int n_classes_;
+        int buffers_size_;
+        float init_val_;
+        dyncfg_buffer recfg_srv_;
+        dyncfg_buffer::CallbackType recfg_callback_type_;
+        bool first_;
+        int index_;
+        std::vector<int> classes_;
+        std::vector<float> init_percentual_;
 };
 
 PLUGINLIB_EXPORT_CLASS(rosneuro::integrator::Buffer, rosneuro::integrator::GenericIntegrator)
 
-	}
+    }
 }
 
 #endif
--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -1,10 +1,10 @@
 #include "rosneuro_integrator_buffer/Buffer.h"
 
 namespace rosneuro {
-	namespace integrator {
+    namespace integrator {
 
 Buffer::Buffer(void) : p_nh_("~") {
-	this->setName("integrator_buffer");
+    this->setName("integrator_buffer");
 }
 
 Buffer::~Buffer(void) {
@@ -12,125 +12,157 @@ Buffer::~Buffer(void) {
 
 bool Buffer::configure(void) {
 
-	int increment, n_classes;
-	std::vector<int> buffers_size;
+    int n_classes;
+    int buffers_size;
+    std::vector<int> classes;
 
-	if(this->p_nh_.getParam("buffers_size", buffers_size) == false) {
-        ROS_ERROR("Parameter 'thresholds' is mandatory");
+    if(this->p_nh_.getParam("buffers_size", buffers_size) == false) {
+        ROS_ERROR("Parameter 'buffers_size' is mandatory");
         return false;
-	}
-	n_classes = buffers_size.size();
-	
-	this->p_nh_.param<int>("increment", increment, INCREMENT_HARD);
+    }
+    if(this->p_nh_.getParam("classes", classes) == false) {
+        ROS_ERROR("Parameter 'classes' is mandatory");
+        return false;
+    }
+    n_classes = classes.size();
 
-	std::vector<float> init_val;
-	// Getting init_val
-	if(this->p_nh_.getParam("init_val", init_val) == false) {
-		ROS_ERROR("Parameter 'init_val' is mandatory");
-		return false;
-	} 
-	else if(init_val.size() != n_classes) {
-		ROS_ERROR("You must specify an initial value for each class. 'n_classes' is %d but 'init_val' has size %d",n_classes,(int)init_val.size());
-		return false;
-	}
+    std::vector<float> init_percentual;
+    if(this->p_nh_.getParam("init_percentual", init_percentual) == false) {
+        ROS_ERROR("Parameter 'init_val' is mandatory");
+        return false;
+    }
+    if(init_percentual.size() != n_classes) {
+        ROS_ERROR("Parameter 'init_percentual' must have the same size of 'classes'");
+        return false;
+    }else if(std::accumulate(init_percentual.begin(), init_percentual.end(), 0.0) != 1.0){
+        ROS_ERROR("Parameter 'init_percentual' must sum to 1.0");
+        return false;
+    }
 
-	// Bind dynamic reconfigure callback
-	this->recfg_callback_type_ = boost::bind(&Buffer::on_request_reconfigure, this, _1, _2);
-	this->recfg_srv_.setCallback(this->recfg_callback_type_);
+    // Bind dynamic reconfigure callback
+    this->recfg_callback_type_ = boost::bind(&Buffer::on_request_reconfigure, this, _1, _2);
+    this->recfg_srv_.setCallback(this->recfg_callback_type_);
 
-	this->setBuffersSize(buffers_size);
-	this->setIncrement(increment);
-	this->setClasses(n_classes);
-	this->setInitVal(init_val);
+    this->setBufferSize(buffers_size);
+    this->setNClasses(n_classes);
+    this->setClasses(classes);
+    this->setInitPercentual(init_percentual);
 
-	this->reset();
+    this->reset();
 
-	this->first_ = false;
-	return true;
+    this->first_ = false;
+    return true;
 }
 
-void Buffer::setClasses(int value){
-	this->n_classes_ = value;
+void Buffer::setClasses(std::vector<int> value){
+    this->classes_ = value;
 }
 
-void Buffer::setIncrement(int value){
-	this-> increment_ = value;
+void Buffer::setNClasses(int value){
+    this->n_classes_ = value;
 }
 
-void Buffer::setBuffersSize(std::vector<int> value){
-	this-> buffers_size_ = value;
+void Buffer::setBufferSize(int value){
+    this-> buffers_size_ = value;
+}
+    
+void Buffer::setInitPercentual(std::vector<float> init_percentual){
+    this->init_percentual_ = init_percentual;
 }
 
-void Buffer::setBufferSize(int value, int idx_buffer){
-	this-> buffers_size_.at(idx_buffer) = value;
-}
-	
-void Buffer::setInitVal(std::vector<float> init_val){
-	this->init_val_ = init_val;
+std::vector<float> Buffer::getInitPrecentual(void){
+    return this->init_percentual_;
 }
 
 Eigen::VectorXf Buffer::apply(const Eigen::VectorXf& input) {
-	float increment_tmp;
-	Eigen::Index maxIndex;
-	if (this->first_ == false){
-		this->first_ =true;
-		ROS_INFO("[integrator_buffer] first frame received");
-	}
-	if(input.size() != this->n_classes_) {
-		ROS_WARN("[%s] Input size (%ld) is not equal to declared input size (%d)", this->name().c_str(),input.size(),this->n_classes_); 
-		return this->data_;
-	}
+    Eigen::Index maxIndex;
+    if (this->first_ == false){
+        this->first_ = true;
+        ROS_INFO("[integrator_buffer] first frame received");
+    }
+    if(input.size() != this->n_classes_) {
+        ROS_ERROR("[%s] Input size (%ld) is not equal to declared input size (%d)", this->name().c_str(),input.size(),this->n_classes_); 
+    }
 
-	input.maxCoeff(&maxIndex);
-	if(this->increment_ == INCREMENT_HARD){
-		increment_tmp = 1.0F; /// this->buffers_size.at(maxIndex);
-	}else if(this->increment_ == INCREMENT_SOFT){
-		increment_tmp = input[maxIndex]; /// this->buffers_size.at(maxIndex);
+	// take the max coefficient of the input vector
+    input.maxCoeff(&maxIndex);
+	if(this->index_ >= this->buffers_size_){
+		this->index_ = 0;
 	}
+	this->buffer_(this->index_) = this->classes_[maxIndex];
+	this->index_ = this->index_ + 1;
 
-	//Increment Buffer
-	for(int i = 0; i < this->n_classes_; i++){
-		if (i == (int)maxIndex){
-			this->data_[i] += (increment_tmp/this->buffers_size_.at(i));
-		}else{
-			this->data_[i] -= (increment_tmp/this->buffers_size_.at(i));
+	// compute the percentage of the classes in the buffer
+	Eigen::VectorXf prob = Eigen::VectorXf::Zero(this->n_classes_);
+	for (int i = 0; i < this->buffers_size_; ++i) {
+		for(int j = 0; j < this->n_classes_; ++j){
+			if(this->buffer_(i) == this->classes_[j]){
+				prob(j) = prob(j) + 1;
+			}
 		}
 	}
-	// [0,1] clipping
-	this->data_ = this->data_.cwiseMax(0.0).cwiseMin(1.0);
 
-	return this->data_;
+	prob = prob / this->buffers_size_;
+
+    return prob;
 }
 
-std::vector<float> Buffer::getInitVal(){
-	return this->init_val_;
-}
+// TODO: keep the percentage of the classes asked in the init_percentual vector, may be the comment part
+bool Buffer::reset(void) { 
+    this->buffer_ = Eigen::VectorXf(this->buffers_size_);
+    for (int i = 0; i < buffers_size_; ++i) {
+        this->buffer_(i) = this->classes_.at(i % this->n_classes_);
+    }
 
-bool Buffer::reset(void) {
-	this->data_ = this->uniform_vector(n_classes_,0);
-	for (int i = 0; i < this->n_classes_; i++){
-		this->data_[i] = this->init_val_.at(i);
-	}
-	return true;
+	this->index_ = 0;
+
+    // compute the percentage of the classes in the buffer
+    /*
+    // Calculate the number of occurrences for each class
+    std::vector<int> counts(this->n_classes_);
+    for (int i = 0; i < this->n_classes_; ++i) {
+        counts[i] = static_cast<int>(this->buffers_size_ * this->init_percentual_[i]);
+    }
+
+    // Adjust the last count to ensure the sum is exactly `size`
+    int sum_counts = std::accumulate(counts.begin(), counts.end(), 0);
+    if (sum_counts != this->buffers_size_) {
+        counts.back() += (this->buffers_size_ - sum_counts);
+    }
+
+    std::vector<float> temp_vector;
+    temp_vector.reserve(this->buffers_size_);
+
+    // Fill the temp_vector with the calculated number of each class value
+    for (int i = 0; i < this->n_classes_; ++i) {
+        temp_vector.insert(temp_vector.end(), counts[i], this->classes_[i]);
+    }
+
+    // Shuffle the temp_vector
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(temp_vector.begin(), temp_vector.end(), g);
+
+    // Transfer the shuffled values to the Eigen vector
+    for (int i = 0; i < this->buffers_size_; ++i) {
+        this->buffer_(i) = temp_vector[i];
+    }
+    std::cout << this->buffer_ << std::endl;
+    */
+
+    return true;
 }
 
 Eigen::VectorXf Buffer::uniform_vector(int size, float value) {
-	return Eigen::VectorXf::Constant(size, value);
+    return Eigen::VectorXf::Constant(size, value);
 }
 
 void Buffer::on_request_reconfigure(rosneuro_config_buffer &config, uint32_t level) {
 
-	if(config.increment != this->increment_){
-		this->setIncrement(config.increment);
-	}
 
-	if(config.buffer_size_0 != this->buffers_size_.at(0)){
-		this->setBufferSize(config.buffer_size_0, 0);
-	}
-
-	if(config.buffer_size_1 != this->buffers_size_.at(1)){
-		this->setBufferSize(config.buffer_size_1, 1);
-	}
+    if(config.buffer_size != this->buffers_size_){
+        this->setBufferSize(config.buffer_size);
+    }
 }
 
 }
